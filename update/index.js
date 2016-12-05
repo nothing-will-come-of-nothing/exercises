9d5d7d70375541ad2cc4a4ec66c1566d55fb7da6
Michael Bagwell <lowghost.clone@gmail.com>
2016-12-05 18:36:37 -0500

    added $set method, 5 tests passing

--- update/index.js
+++ update/index.js
@@ -3,18 +3,73 @@
 // back rather than undefined
 const isKeyOf = (key, obj) => (obj && obj.hasOwnProperty && obj.hasOwnProperty(key)) || false;
 
-// We'll also need to check if commandSet key is one of the accepted commands
-const isCommand = (key) => {
+// Just for testing purposes...
+const id = (x) => { console.log('called func with', x); return x };
 
-  const commands = [ '$set', '$unshift', '$splice', '$merge', '$push', 'hasOwnProperty' ];
+// This is better than an array for membership checks, because we can access the function directly
+const commandFunctions = {
+  $set: (command, toUpdate) => command.value || toUpdate,
+  $unshift: id,
+  $splice: id,
+  $merge: id,
+  $push: id,
+  hasOwnProperty: id,
+};
 
-  return commands.indexOf(key) > -1;
+// We actually need to reduce the commandSet to see if it has any command keys we're looking for.
+// If it does, we'll return an object with everything necessary to perform the command
+// I don't know what facebook does, but I assume providing multiple update keys will be problematic
+// and I'll just select one of them and warn the user
+const getCommand = (commandSet) => {
+
+  return Object.keys(commandSet).reduce(
+    (acc, key) => {
+
+      // Get the command function from the above object via prop lookup (I made the object, so
+      // no need for hasOwnProperty dance)
+      const func = commandFunctions[key];
+
+      if (!func)
+        return acc;
+
+      const hasMultiple = acc.key && true;
+
+      return { key, value: commandSet[key], hasMultiple, func };
+
+    },
+    {}
+  );
 
 };
 
-const recursiveReduceCommandSet = (toUpdate, commandSet) => {
+const update = (toUpdate, commandSet) => {
 
-  // First step is going to be reducing the toUpdate object immutably
+  // If a command exists directly on the commandSet, we can go ahead and perform it here
+  // rather than initiating the toUpdate reducer
+  const command = getCommand(commandSet);
+  console.log(command)
+
+  if (typeof toUpdate !== 'object' || command.key) {
+
+    if (command.hasMultiple) {
+
+      console.warn(
+        'You should only provide one command to the update function.'
+        + ' We will just choose one for now and let you go with this warning'
+      );
+
+    }
+
+    // Perform the command if it returned successfully
+    if (command.key)
+      return command.func(command, toUpdate);
+
+    // Otherwise, just return the object itself
+    return toUpdate;
+
+  }
+
+  // Reduce the toUpdate object immutably
   const toUpdateKeys = Object.keys(toUpdate);
 
   return toUpdateKeys.reduce(
@@ -26,7 +81,7 @@ const recursiveReduceCommandSet = (toUpdate, commandSet) => {
       // This will be a recursive call if commandSet key is a key of the updating object
       if (isKeyResult) {
 
-        const newValue = recursiveReduceCommandSet(value, commandSet[key]);
+        const newValue = update(value, commandSet[key]);
 
         // This could basically be acc[key] = recursiveReduce..., but I prefer reducers to avoid
         // mutation even at a slight cost to legibility
@@ -34,15 +89,8 @@ const recursiveReduceCommandSet = (toUpdate, commandSet) => {
 
       }
 
-      // We need to update the value at this point if commandSet has a command key
-      if (isCommand(key)) {
-
-        // Hand wavy do some stuff here
-        // return Object.assign({}, acc, { [key]: someFunc(newValue) });
-
-      }
-
       // Key is to be ignored and thus simply set to the original value
+      console.log('Key is to be ignored and thus simply set to the original value');
       return Object.assign({}, acc, { [key]: value });
 
     },
@@ -51,10 +99,5 @@ const recursiveReduceCommandSet = (toUpdate, commandSet) => {
 
 };
 
-const update = (toUpdate, commandSet) => {
-
-  // Do some things if necessary before calling recursive function
-
-};
-
-exports.default = update;
+// Default export, at least pre `export default update` world
+module.exports = update;
