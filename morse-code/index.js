fa6459e4e3b804253e96940f89b6679eabf7ddb9
Michael Bagwell <lowghost.clone@gmail.com>
2016-12-05 22:45:54 -0500

    added base constants. now to do something with them

--- morse-code/index.js
+++ morse-code/index.js
@@ -1,21 +1,27 @@
 "use strict"
+// 1. dot duration is the baseline timing measurement
+const dot = 1;
+// 2. dashes are timed as 3 dots
+const dash = 3;
+// 3. time between letters are 3 dots
+const letters = 3;
+// 4. time between words are 7 dots
+const words = 7;
 
-// Maybe we don't need some of these props, let me think on it
-const getTranslated = (props) => {
+const getTranslated = (codes, message) => {
 
-  // Do stuff
+  return message.split().reduce(something, '');
 
 };
 
 const transmitter = (props, cb) => {
 
-  // We don't have object destructure just yet
   const codes = props.codes;
   const message = props.message;
-  const timeouter = props.timeouter;
   const toggle = props.toggle;
+  const timeouter = props.timeouter;
 
-  const translated = getTranslated(props);
+  const translated = getTranslated(codes, message);
 
   const transmit = (all) => {
 
@@ -23,19 +29,18 @@ const transmitter = (props, cb) => {
     toggle();
 
     // Get head rest. Could be `const [ head, ...rest ] = all;` in later nodes
-    const [ head, rest ] = [ all[0], all.slice(1) ];
-    const isDone = !rest.length;
+    const head = all[0]
+    const rest = all.slice(1);
+    const isDone = !all.length;
     // Either we're done and can call user provided cb, or we have to transmit via the timeouter
     const newCb = isDone ? cb : () => transmit(rest);
 
     return isDone ? newCb() : timeouter(newCb, head);
 
-  };
+  }
 
-  // Transmit the translated message, will be recursive
   return transmit(translated);
 
 };
-
 
 module.exports = transmitter;
