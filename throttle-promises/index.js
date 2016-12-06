bf5877d84fb78490431881008f3590399515dfdd
Michael Bagwell <lowghost.clone@gmail.com>
2016-12-05 20:32:22 -0500

    started throttle-promises

---
+++ throttle-promises/index.js
@@ -0,0 +1,12 @@
+const throttlePromises = (num, arr) => {
+
+  // We'll wrap everything in a promise
+  return new Promise((resolve, reject) => {
+
+    // Do Some Stuff
+
+  });
+
+};
+
+module.exports = throttlePromises;
