88e8524b049443b372d6550799bfb19fdf8d022d
Michael Bagwell <lowghost.clone@gmail.com>
2016-12-05 22:00:53 -0500

    starting morse-code

---
+++ morse-code/index.js
@@ -0,0 +1,14 @@
+"use strict"
+
+const transmitter = (props, cb) => {
+
+  // We don't have object destructure just yet
+  const codes = props.codes;
+  const message = props.message;
+  const timeouter = props.timeouter;
+  const toggle = props.toggle;
+
+};
+
+
+module.exports = transmitter;
